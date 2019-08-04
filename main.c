c763602aababa70858e19e2d1159550c87ebb368
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2019-08-03 19:49:54 -0700

    Improve terminal output; reverse order of lines

--- main.c
+++ main.c
@@ -24,34 +24,43 @@ int coin_throw()
 
     printf("%s ", coin);
   }
-  printf("\n");
+  printf(" (%d)\n", total);
   return total;
 }
 
 int main()
 {
-  printf("\n -- C-Ching --\n");
+  printf("\n -- The C-Ching --\n\n");
   char trigram[100];
+  int throws[6];
   int total;
 
+  // Throw coin 6 times
   for (int i = 0; i <= 6; i++) {
-    printf("Throw %d\n", i);
+    printf("Throw %d: ", i);
     total = coin_throw();
-    printf("Total: %d\n\n", total);
+    throws[i] = total;
+    // Sleep between throws or else random number
+    // will be the same for each throw.
+    sleep(1);
+  }
+  printf("\n");
 
-    if (total % 2 == 0) {
+  for (int j = 5; j >= 0; j--) {
+    if (throws[j] % 2 == 0) {
       strcat(trigram, "---------");
     }
     else {
       strcat(trigram, "---   ---");
     }
 
-    if (total % 3 == 0) {
+    if (throws[j] % 3 == 0) {
       strcat(trigram, " *");
     }
+
     strcat(trigram, "\n");
-    sleep(1);
   }
+
   printf("%s\n", trigram);
   return 0;
 }
